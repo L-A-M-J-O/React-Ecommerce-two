dc2b57210cd1d6305fc123205f3ece1d8b897744
L-A-M-J-O <lamjo30@gmail.com>
2022-11-17 20:05:36 +0100

    Push

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Item from './components/Item/Item';
 import { Navbar } from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar/> 
-      <Item/>
     </div>
 
   );
