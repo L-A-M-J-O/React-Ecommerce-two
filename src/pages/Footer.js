f97eef4ed42bae4574b85af0d71edf2ccb5e13ba
L-A-M-J-O <lamjo30@gmail.com>
2022-12-03 21:53:19 +0100

    Finish Home

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import '../styles/Footer.css';
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faPaperclip} from '@fortawesome/free-solid-svg-icons'
+import { faGithub , faLinkedin} from '@fortawesome/free-brands-svg-icons';
 
 export const Footer = () => {
   return (
     <>
         <footer className='text-xs text-white'>
             <div className='row justify-content-center p-0 m-0'>
-                <div className='col-4 text-center'>
+                <div className='col-12 text-center'>
                     <small>&copy; 2022 <b>Alan Mateo Naranjo Alvarado</b></small>
                     <br/>
+                    <div className='row justify-center'>
+                    <div className='col-auto p-1'>
+                        <a href='https://github.com/L-A-M-J-O'>
+                            <FontAwesomeIcon icon={faGithub} />
+                        </a>
+                    </div>
+                    <div className='col-auto p-1'>
+                        <a href='https://www.linkedin.com/in/alann3009200330092003/'>
+                            <FontAwesomeIcon icon={faLinkedin} />
+                        </a>
+                    </div>
+                    <div className='col-auto p-1'>
+                        <a href='https://portfolio-alan-naranjo.vercel.app/'>
+                            <FontAwesomeIcon icon={faPaperclip} />
+                        </a>
+                    </div>
+                    </div>
+                    <br/>
                     <a href='https://www.adultswim.com/'><i>Adult Swim Pagina Original</i></a>
-                    <FontAwesomeIcon icon="fa-brands fa-square-facebook" />
+        
                 </div>
-                <div className='col-6 m-2'>
+                <div className='col-12 m-2 text-center'>
                     SIGN UP FOR THE ADULT SWIM NEWSLETTER
                 </div>
                 <div className='col-12 text-center'>
