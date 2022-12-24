e4833b5b25d23125e66dfe0373e7c72529f72a7d
L-A-M-J-O <lamjo30@gmail.com>
2022-12-24 15:30:06 +0100

    cambios

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -3,19 +3,8 @@ import { Link } from 'react-router-dom';
 
 const Item = ({id, name, image}) => {
   return (
-    <>
-      <div class="card">
-        <h3 class="title">{name}</h3>
-        <img className='img-banner' src={image} alt='the character'/>
-        {/* <div class="bar">
-          <div class="emptybar"></div>
-          <div class="filledbar"></div>
-        </div> */}
-        <Link to={`/detail/${id}`}>
-          ENTrAR
-        </Link>
-      </div>
-    </>
+    <div class="col-lg-3">
+    </div>
   )
 }
 
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -3,7 +3,7 @@ import Item from '../Item/Item'
 const ItemList = ({character}) => {
   console.log(character)
   return (
-    <div className='row justify-conent-center mt-5'>
+    <div className='row justify-content-center mt-5 box-items'>
         {character.map((element) => <Item key={element.id} {...element}/>)}
     </div>
   )
