2debbc563624d0fa2e3a7e5ed32666dc794180d1
L-A-M-J-O <lamjo30@gmail.com>
2022-12-25 01:28:42 +0100

    cambios

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,31 +1,39 @@
 import '../../components/Item/Item.css';
 import { Link } from 'react-router-dom';
 
-const Item = ({id, name, image}) => {
+const Item = ({id, name, image, specie, origin, status}) => {
+
+    const active = 0;
+    const desactive = 0;
+
+    if(status >= 1){
+
+    }
+
   return (
-    <div class="col-lg-4">
-        <div class="card card-margin">
-            <div class="card-header no-border">
-                <h5 class="card-title">{name}</h5>
+    <div className="col-lg-4">
+        <div className="card card-margin">
+            <div className="card-header no-border">
+                <h5 className="card-title">{name}</h5>
             </div>
-            <div class="card-body pt-0">
-                <div class="widget-49">
-                    <div class="widget-49-title-wrapper">
-                        <div class="widget-49-date-primary">
+            <div className="card-body pt-0 bg-white">
+                <div className="widget-49">
+                    <div className="widget-49-title-wrapper bg-white">
+                        <div className="widget-49-date-primary bg-white mb-3">
                           <img src={image} alt='character'/>
                         </div>
-                        <div class="widget-49-meeting-info">
-                            <span class="widget-49-pro-title">PRO-08235 DeskOpe. Website</span>
-                            <span class="widget-49-meeting-time">12:00 to 13.30 Hrs</span>
+                        <div className="widget-49-meeting-info">
+                            <span className="widget-49-pro-title bg-white">{specie}</span>
+                            <span className="widget-49-meeting-time bg-white">{origin}</span>
                         </div>
                     </div>
-                    <ol class="widget-49-meeting-points">
-                        <li class="widget-49-meeting-item"><span>Expand module is removed</span></li>
-                        <li class="widget-49-meeting-item"><span>Data migration is in scope</span></li>
-                        <li class="widget-49-meeting-item"><span>Session timeout increase to 30 minutes</span></li>
+                    <ol className="widget-49-meeting-points bg-white">
+                    <li className="widget-49-meeting-item bg-white"><span className='bg-white text-black'>Expand module is removed</span></li>
+                        <li className="widget-49-meeting-item bg-white"><span className='bg-white text-black'>Data migration is in scope</span></li>
+                        <li className="widget-49-meeting-item bg-white"><span className='bg-white text-black'>Session timeout increase to 30 minutes</span></li>
                     </ol>
-                    <div class="widget-49-meeting-action">
-                        <a href="#" class="btn btn-sm btn-flash-border-primary">View All</a>
+                    <div className="widget-49-meeting-action bg-white">
+                        <Link href="#" className="btn btn-sm btn-flash-border-primary">View All</Link>
                     </div>
                 </div>
             </div>
