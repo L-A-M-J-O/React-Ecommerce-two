c324640843d1515621213863d977f09b76b3843c
L-A-M-J-O <lamjo30@gmail.com>
2022-12-26 13:25:37 +0100

    uush

--- src/components/CartCharacter/CartChracter.js
+++ src/components/CartCharacter/CartChracter.js
@@ -1,7 +1,92 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import CartContext from '../../context/context';
 
 export const CartChracter = () => {
+
+  const { cart, clearCart, removeItem, total } = useContext(CartContext);
+
   return (
-    <div className='text-white'>CartChracter</div>
+		<>
+			{cart.length === 0 ? (
+				<>
+					<h1 className="text-center mt-4">Your cart is empty</h1>
+					<div className="row justify-content-center mt-3">
+						<div className="col-12 col-sm-12 col-md-5 col-lg-5 text-center">
+							<Link to="/cervezas" className="m-txt-a" aria-current="page">
+								<button className="btnB mt-4">CERVEZAS</button>
+							</Link>
+						</div>
+					</div>
+				</>
+			) : (
+				<>
+					<h1 className="text-center mt-4">Your purchase</h1>
+					<div className="cart mt-4">
+							<thead>
+								<tr>
+									<th />
+									<th>Products</th>
+									<th>Price</th>
+									<th>Quantity</th>
+									<th>Total</th>
+								</tr>
+							</thead>
+							<tbody>
+								{cart.map((guia) => {
+									return (
+										<tr key={guia.id}>
+											<td>
+												<img
+													width={28}
+													src=''
+													alt="borrar"
+													onClick={() => removeItem(guia.id)}
+													id={guia.id}
+												/>
+											</td>
+											<td>
+												<Link
+													className="n-a linkProduct "
+													to={`../Detail/${guia.id}`}
+												>
+													{guia.name}
+												</Link>
+											</td>
+											<td>${guia.price}</td>
+											<td>{guia.quantity}</td>
+											<td>${guia.total}</td>
+										</tr>
+									);
+								})}
+								<tr>
+									<td colSpan={4}>Total</td>
+									<td>${total}</td>
+								</tr>
+							</tbody>
+						{cart.length !== 0 && (
+							<div className="row justify-content-center">
+								<div className="col-2">
+									<button className="btnI" onClick={clearCart}>
+										Clean cart
+									</button>
+								</div>
+								<div className="col-2">
+									<Link className="m-a-c" to="/cervezas">
+										Keep buying
+									</Link>
+								</div>
+								<div className="col-2">
+									<Link to="/form">
+										<button className="btnI">To buy</button>
+									</Link>
+								</div>
+							</div>
+						)}
+					</div>
+				</>
+			)}
+		</>
   )
 }
--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -32,24 +32,24 @@ const ItemDetail = ({name, image, id, origin, specie, status, genere, episode}) 
   return (
     <>
 		<h1>{name}</h1>
-		<div className="min-h-screen w-full">
-			<div className="max-w-screen-md mx-auto px-10 pt-20">
-				<div className="md:h-48 rounded-lg shadow-md flex flex-wrap flex-col-reverse md:flex-col">
-					<div className="w-full md:w-1/2 p-4">
-						<h3 className="text-3xl font-bold">{specie}</h3>
-						<p className=''>{origin}</p>
-						<p className=''>{genere}</p>
-						<p className='text-5xl mt-2'>{status}</p>
-						<p>	incl. 19% VAT. (the VAT of the respective EU country will be calculated at the checkout.) plus</p>
-						<div>{quantityAdd <= 0 ? (
-						<ItemCount status={status} initial={productQuantity} onAdd={handleOnAdd}/>) : ( <div className='row justify-content-center'><div className='col-5'><Link to='/cartCharacter'><button className='btnB mt-4'>Buy</button></Link></div><div className='col-5'><Link to='/personajes'><button className='btnB mt-4'>Characters</button></Link></div></div>)}</div>
-					</div>
-					<div className="w-full md:w-1/2 p-4 md:p-0">
-						<img src={image} alt="" className="w-100 mx-auto"/>
+			<div className="min-h-screen w-full card-box">
+				<div className="max-w-screen-md mx-auto px-10 pt-20">
+					<div className="md:h-48 rounded-lg shadow-md flex flex-wrap flex-col-reverse md:flex-col">
+						<div className="w-full md:w-1/2 p-4">
+							<h3 className="text-3xl font-bold">{specie}</h3>
+							<p className=''>{origin}</p>
+							<p className=''>{genere}</p>
+							<p className='text-5xl mt-2'>{status}</p>
+							<p>	incl. 19% VAT. (the VAT of the respective EU country will be calculated at the checkout.) plus</p>
+							<div>{quantityAdd <= 0 ? (
+							<ItemCount status={status} initial={productQuantity} onAdd={handleOnAdd}/>) : ( <div className='row justify-content-center'><div className='col-5'><Link to='/cartCharacter'><button className='btnB mt-4'>Buy</button></Link></div><div className='col-5'><Link to='/personajes'><button className='btnB mt-4'>Characters</button></Link></div></div>)}</div>
+						</div>
+						<div className="w-full md:w-1/2 p-4 md:p-0">
+							<img src={image} alt="" className="w-100 mx-auto"/>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
     </>
   )
 }
